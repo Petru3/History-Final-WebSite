4f3ee75d824c758b46ad8fac9359c3d395f78d2e
P3tru.js <98655629+Petru3@users.noreply.github.com>
2023-02-26 16:15:44 +0200

    Add files via upload

---
+++ Istorie/JS/app.js
@@ -0,0 +1,15 @@
+const loader = document.querySelector('.loader_bg');
+
+setTimeout(function(){
+    $('.loader_bg').fadeToggle();
+}, 1000);
+
+$(document).ready(function() {
+    $(document).on('mousemove', function(e) {
+      $('#cursor').css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    })
+});
+
